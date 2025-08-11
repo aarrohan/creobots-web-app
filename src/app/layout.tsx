3b0381c58661d791a1748102f77a26077b5be34d
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2025-08-11 06:29:30 +0600

    feat: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import PaddleProvider from "@/components/providers/PaddleProvider";
 
 const inter = Inter({
@@ -21,6 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${inter.className} antialiased`}>
         <PaddleProvider>{children}</PaddleProvider>
+
+        <Analytics />
       </body>
     </html>
   );
